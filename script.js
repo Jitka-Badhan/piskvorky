fba042733352d9adc4d391536c2366e24f0560e6
Jitka Badhan Nebeska <jitkabadhan@gmail.com>
2021-04-29 11:23:22 +0200

    Buttons disabled after click in JS (removeEventListener method not needful anymore), animation in CSS

--- script.js
+++ script.js
@@ -3,22 +3,22 @@
 let player = 'circle';
 
 const drawCrossOrCircle = (event) => {
-  const selectedButton = event.toElement;
+  const selectedButton = event.target;
   const playerIcon = document.querySelector('.play__nav__player > img');
 
   if (player === 'circle') {
     selectedButton.classList.add('board__field--circle');
+    selectedButton.disabled = true;
     player = 'cross';
     playerIcon.setAttribute('src', 'img/cross.svg');
   } else if (player === 'cross') {
     selectedButton.classList.add('board__field--cross');
+    selectedButton.disabled = true;
     player = 'circle';
     playerIcon.setAttribute('src', 'img/circle.svg');
   } else {
     console.log('Error inside function drawCrossOrCircle');
   }
-
-  selectedButton.removeEventListener('click', drawCrossOrCircle);
 };
 
 document.querySelectorAll('button').forEach((item) => {
